8de478cd66352e6f28489bed6764ba4e2853f2d8
0812638 <0812638@gmail.com>
2011-03-23 20:36:32 +0000

    Nhom_22/SourceCUAHANGLAPTOP

---
+++ Nhom_22/SourceCUAHANGLAPTOP/QLKS/THONGTINNHOM.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace QLKS
+{
+    partial class THONGTINNHOM
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelThongTinNhom = new DevComponents.DotNetBar.Controls.GroupPanel();
+            this.buttonThoat = new DevComponents.DotNetBar.ButtonX();
+            this.labelControlTruong = new DevExpress.XtraEditors.LabelControl();
+            this.labelControlThongTinNhom = new DevExpress.XtraEditors.LabelControl();
+            this.panelThongTinNhom.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelThongTinNhom
+            // 
+            this.panelThongTinNhom.AutoSize = true;
+            this.panelThongTinNhom.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.panelThongTinNhom.CanvasColor = System.Drawing.SystemColors.Control;
+            this.panelThongTinNhom.ColorSchemeStyle = DevComponents.DotNetBar.eDotNetBarStyle.Office2007;
+            this.panelThongTinNhom.Controls.Add(this.buttonThoat);
+            this.panelThongTinNhom.Controls.Add(this.labelControlTruong);
+            this.panelThongTinNhom.Controls.Add(this.labelControlThongTinNhom);
+            this.panelThongTinNhom.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelThongTinNhom.Location = new System.Drawing.Point(0, 0);
+            this.panelThongTinNhom.Name = "panelThongTinNhom";
+            this.panelThongTinNhom.Size = new System.Drawing.Size(876, 266);
+            // 
+            // 
+            // 
+            this.panelThongTinNhom.Style.BackColor2SchemePart = DevComponents.DotNetBar.eColorSchemePart.PanelBackground2;
+            this.panelThongTinNhom.Style.BackColorGradientAngle = 90;
+            this.panelThongTinNhom.Style.BackColorSchemePart = DevComponents.DotNetBar.eColorSchemePart.PanelBackground;
+            this.panelThongTinNhom.Style.BorderBottom = DevComponents.DotNetBar.eStyleBorderType.Solid;
+            this.panelThongTinNhom.Style.BorderBottomWidth = 1;
+            this.panelThongTinNhom.Style.BorderColorSchemePart = DevComponents.DotNetBar.eColorSchemePart.PanelBorder;
+            this.panelThongTinNhom.Style.BorderLeft = DevComponents.DotNetBar.eStyleBorderType.Solid;
+            this.panelThongTinNhom.Style.BorderLeftWidth = 1;
+            this.panelThongTinNhom.Style.BorderRight = DevComponents.DotNetBar.eStyleBorderType.Solid;
+            this.panelThongTinNhom.Style.BorderRightWidth = 1;
+            this.panelThongTinNhom.Style.BorderTop = DevComponents.DotNetBar.eStyleBorderType.Solid;
+            this.panelThongTinNhom.Style.BorderTopWidth = 1;
+            this.panelThongTinNhom.Style.CornerDiameter = 4;
+            this.panelThongTinNhom.Style.CornerType = DevComponents.DotNetBar.eCornerType.Rounded;
+            this.panelThongTinNhom.Style.TextAlignment = DevComponents.DotNetBar.eStyleTextAlignment.Center;
+            this.panelThongTinNhom.Style.TextColorSchemePart = DevComponents.DotNetBar.eColorSchemePart.PanelText;
+            this.panelThongTinNhom.Style.TextLineAlignment = DevComponents.DotNetBar.eStyleTextAlignment.Near;
+            this.panelThongTinNhom.TabIndex = 1;
+            this.panelThongTinNhom.Text = "Thông Tin Nhóm";
+            // 
+            // buttonThoat
+            // 
+            this.buttonThoat.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
+            this.buttonThoat.ColorTable = DevComponents.DotNetBar.eButtonColor.OrangeWithBackground;
+            this.buttonThoat.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonThoat.Location = new System.Drawing.Point(404, 213);
+            this.buttonThoat.Name = "buttonThoat";
+            this.buttonThoat.Size = new System.Drawing.Size(75, 23);
+            this.buttonThoat.TabIndex = 11;
+            this.buttonThoat.Text = "Thoát";
+            this.buttonThoat.Click += new System.EventHandler(this.buttonThoat_Click);
+            // 
+            // labelControlTruong
+            // 
+            this.labelControlTruong.Appearance.Font = new System.Drawing.Font("Tahoma", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelControlTruong.Appearance.Options.UseFont = true;
+            this.labelControlTruong.Location = new System.Drawing.Point(47, 98);
+            this.labelControlTruong.Name = "labelControlTruong";
+            this.labelControlTruong.Size = new System.Drawing.Size(394, 24);
+            this.labelControlTruong.TabIndex = 10;
+            this.labelControlTruong.Text = "Trường Đại Học Khoa Học Tự Nhiên TP.HCM";
+            // 
+            // labelControlThongTinNhom
+            // 
+            this.labelControlThongTinNhom.Appearance.Font = new System.Drawing.Font("Tahoma", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelControlThongTinNhom.Appearance.Options.UseFont = true;
+            this.labelControlThongTinNhom.Location = new System.Drawing.Point(47, 59);
+            this.labelControlThongTinNhom.Name = "labelControlThongTinNhom";
+            this.labelControlThongTinNhom.Size = new System.Drawing.Size(379, 24);
+            this.labelControlThongTinNhom.TabIndex = 9;
+            this.labelControlThongTinNhom.Text = "Nhóm lớn 3 - Bộ Môn Xây Dựng Phần Mềm";
+            // 
+            // THONGTINNHOM
+            // 
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(867, 266);
+            this.Controls.Add(this.panelThongTinNhom);
+            this.MaximizeBox = false;
+            this.Name = "THONGTINNHOM";
+            this.Text = "Thông tin nhóm";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.panelThongTinNhom.ResumeLayout(false);
+            this.panelThongTinNhom.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DevComponents.DotNetBar.Controls.GroupPanel panelThongTinNhom;
+        private DevExpress.XtraEditors.LabelControl labelControlThongTinNhom;
+        private DevExpress.XtraEditors.LabelControl labelControlTruong;
+        private DevComponents.DotNetBar.ButtonX buttonThoat;
+    }
+}
