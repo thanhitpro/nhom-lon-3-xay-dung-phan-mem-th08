35d3c0b24bd318a0007862f006e22bfbd3418076
0812491 <0812491@8247ae65-48d3-9fc0-23df-3783ca5f111f>
2011-03-24 11:43:58 +0000

    Nhóm 24 up source lần 1

---
+++ Nhom_24/EStore/QLKS/TATCASANPHAM.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace QLKS
+{
+    partial class TATCASP
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TATCASP));
+            this.flp_TatCaSanPham = new System.Windows.Forms.FlowLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // flp_TatCaSanPham
+            // 
+            this.flp_TatCaSanPham.AutoScroll = true;
+            this.flp_TatCaSanPham.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flp_TatCaSanPham.Location = new System.Drawing.Point(0, 0);
+            this.flp_TatCaSanPham.Name = "flp_TatCaSanPham";
+            this.flp_TatCaSanPham.Size = new System.Drawing.Size(792, 466);
+            this.flp_TatCaSanPham.TabIndex = 0;
+            // 
+            // TATCASP
+            // 
+            this.ClientSize = new System.Drawing.Size(792, 466);
+            this.Controls.Add(this.flp_TatCaSanPham);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "TATCASP";
+            this.Text = "TẤT CẢ CÁC SẢN PHẨM TRONG ESTORE";
+            this.Load += new System.EventHandler(this.TATCASP_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flp_TatCaSanPham;
+
+
+    }
+}
