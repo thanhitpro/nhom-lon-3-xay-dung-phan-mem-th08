aeac42c777768e75823d83b0b8c33c7b9d68d3b5
0812491 <0812491@8247ae65-48d3-9fc0-23df-3783ca5f111f>
2011-03-24 11:43:58 +0000

    Nhóm 24 up source lần 1

---
+++ trunk/Nhom_24/EStore/EStoreBUS/myDongLaptopBUS.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using EStoreDAO;
+using EStoreDTO;
+
+namespace EStoreBUS
+{
+    public class myDongLaptopBUS
+    {
+        /// <summary>
+        /// Thuộc tính quản lý các công việc với CSDL
+        /// </summary>
+        private myDongLaptopDAO m_dDongLaptopDao = new myDongLaptopDAO();
+
+        /// <summary>
+        /// Hàm trả về danh sách các dòng laptop từ DAO
+        /// </summary>
+        public List<myDongLaptop> LayDSDongLaptop()
+        {
+            return m_dDongLaptopDao.LayDSDongLaptop();
+        }
+
+        /// <summary>
+        /// Hàm trả về danh sách các dòng laptop thuộc 1 nhà sản xuất
+        /// </summary>
+        public List<myDongLaptop> LayDSDongLaptop(int _iMaNhaSX)
+        {
+            return m_dDongLaptopDao.LayDSDongLaptop(_iMaNhaSX);
+        }
+    }
+}
---
+++ trunk/Nhom_24/EStore/EStoreDTO/myDongLaptop.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EStoreDTO
+{
+    public class myDongLaptop
+    {
+        private int m_iMaDong;
+
+        public int IMaDong
+        {
+            get { return m_iMaDong; }
+            set { m_iMaDong = value; }
+        }
+
+        private string m_sTenDong;
+
+        public string STenDong
+        {
+            get { return m_sTenDong; }
+            set { m_sTenDong = value; }
+        }
+
+        private int m_iMaDongRAM;
+
+        public int IMaDongRAM
+        {
+            get { return m_iMaDongRAM; }
+            set { m_iMaDongRAM = value; }
+        }
+
+        private int m_iMaDongCPU;
+
+        public int IMaDongCPU
+        {
+            get { return m_iMaDongCPU; }
+            set { m_iMaDongCPU = value; }
+        }
+
+        private int m_iMaDongOCung;
+
+        public int IMaDongOCung
+        {
+            get { return m_iMaDongOCung; }
+            set { m_iMaDongOCung = value; }
+        }
+
+        private int m_iMaDongManHinh;
+
+        public int IMaDongManHinh
+        {
+            get { return m_iMaDongManHinh; }
+            set { m_iMaDongManHinh = value; }
+        }
+
+        private int m_iMaDongCardDoHoa;
+
+        public int IMaDongCardDoHoa
+        {
+            get { return m_iMaDongCardDoHoa; }
+            set { m_iMaDongCardDoHoa = value; }
+        }
+
+        private int m_iMaDongLoa;
+
+        public int IMaDongLoa
+        {
+            get { return m_iMaDongLoa; }
+            set { m_iMaDongLoa = value; }
+        }
+
+        private int m_iMaDongODiaQuang;
+
+        public int IMaDongODiaQuang
+        {
+            get { return m_iMaDongODiaQuang; }
+            set { m_iMaDongODiaQuang = value; }
+        }
+
+        private int m_iCardMang;
+
+        public int ICardMang
+        {
+            get { return m_iCardMang; }
+            set { m_iCardMang = value; }
+        }
+
+        private int m_iMaDongCardReader;
+
+        public int IMaDongCardReader
+        {
+            get { return m_iMaDongCardReader; }
+            set { m_iMaDongCardReader = value; }
+        }
+
+        private int m_iMaDongWebcam;
+
+        public int IMaDongWebcam
+        {
+            get { return m_iMaDongWebcam; }
+            set { m_iMaDongWebcam = value; }
+        }
+
+        private int m_iMaDongPin;
+
+        public int IMaDongPin
+        {
+            get { return m_iMaDongPin; }
+            set { m_iMaDongPin = value; }
+        }
+
+        private int m_iMaHeDieuHanh;
+
+        public int IMaHeDieuHanh
+        {
+            get { return m_iMaHeDieuHanh; }
+            set { m_iMaHeDieuHanh = value; }
+        }
+
+        private int m_iMaTrongLuong;
+
+        public int IMaTrongLuong
+        {
+            get { return m_iMaTrongLuong; }
+            set { m_iMaTrongLuong = value; }
+        }
+
+        private bool m_bFingerprintReader;
+
+        public bool BFingerprintReader
+        {
+            get { return m_bFingerprintReader; }
+            set { m_bFingerprintReader = value; }
+        }
+
+        private bool m_bHDMI;
+
+        public bool IHDMI
+        {
+            get { return m_bHDMI; }
+            set { m_bHDMI = value; }
+        }
+
+        private int m_iSoCongUSB;
+
+        public int ISoCongUSB
+        {
+            get { return m_iSoCongUSB; }
+            set { m_iSoCongUSB = value; }
+        }
+
+        private myNhaSX m_nMaNhaSX;
+
+        public myNhaSX NMaNhaSX
+        {
+            get { return m_nMaNhaSX; }
+            set { m_nMaNhaSX = value; }
+        }
+
+        private int m_iMaDanhGia;
+
+        public int IMaDanhGia
+        {
+            get { return m_iMaDanhGia; }
+            set { m_iMaDanhGia = value; }
+        }
+
+        private float m_fGiaBanHienHanh;
+
+        public float FGiaBanHienHanh
+        {
+            get { return m_fGiaBanHienHanh; }
+            set { m_fGiaBanHienHanh = value; }
+        }
+
+        private string m_sMoTaThem;
+
+        public string SMoTaThem
+        {
+            get { return m_sMoTaThem; }
+            set { m_sMoTaThem = value; }
+        }
+
+        private int m_iSoLuongNhap;
+
+        public int ISoLuongNhap
+        {
+            get { return m_iSoLuongNhap; }
+            set { m_iSoLuongNhap = value; }
+        }
+
+        private int m_iSoLuongConLai;
+
+        public int ISoLuongConLai
+        {
+            get { return m_iSoLuongConLai; }
+            set { m_iSoLuongConLai = value; }
+        }
+
+        private int m_iThoiGianBaoHanh;
+
+        public int IThoiGianBaoHanh
+        {
+            get { return m_iThoiGianBaoHanh; }
+            set { m_iThoiGianBaoHanh = value; }
+        }
+
+        private string m_sHinhAnh;
+
+        public string SHinhAnh
+        {
+            get { return m_sHinhAnh; }
+            set { m_sHinhAnh = value; }
+        }
+
+        private string m_sMauSac;
+
+        public string SMauSac
+        {
+            get { return m_sMauSac; }
+            set { m_sMauSac = value; }
+        }
+    }
+}
