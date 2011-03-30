7b615f478ed6ead8225ca0d2210629bd50348096
0812451@gmail.com <0812451@gmail.com@8247ae65-48d3-9fc0-23df-3783ca5f111f>
2011-03-30 15:12:34 +0000

    0812451 Commit DTO.

---
+++ trunk/Nhom_29/SourceCUAHANGLAPTOP/EStoreDTO/myChiTietBoNhoRamDTO.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EStoreDTO
+{
+    class myChiTietBoNhoRamDTO
+    {
+        string m_sTenChiTietBoNhoRam;
+
+        public string STenChiTietBoNhoRam
+        {
+            get { return m_sTenChiTietBoNhoRam; }
+            set { m_sTenChiTietBoNhoRam = value; }
+        }
+
+        float m_fHeSo;
+
+        public float FHeSo
+        {
+            get { return m_fHeSo; }
+            set { m_fHeSo = value; }
+        }
+    }
+}
