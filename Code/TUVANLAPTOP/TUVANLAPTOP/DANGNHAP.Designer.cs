99bc54a001a52e75402c5a9640f192ac2f8aa80b
0812365 <0812365@gmail.com>
2011-05-04 18:47:56 +0000

    Nhóm 25 - Bổ sung Progress Bar

--- Code/TUVANLAPTOP/TUVANLAPTOP/DANGNHAP.Designer.cs
+++ Code/TUVANLAPTOP/TUVANLAPTOP/DANGNHAP.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DANGNHAP));
             this.label_username = new System.Windows.Forms.Label();
             this.label_password = new System.Windows.Forms.Label();
             this.textBox_username = new System.Windows.Forms.TextBox();
@@ -40,7 +41,7 @@
             // 
             this.label_username.AutoSize = true;
             this.label_username.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label_username.Location = new System.Drawing.Point(36, 35);
+            this.label_username.Location = new System.Drawing.Point(52, 35);
             this.label_username.Name = "label_username";
             this.label_username.Size = new System.Drawing.Size(61, 14);
             this.label_username.TabIndex = 0;
@@ -50,7 +51,7 @@
             // 
             this.label_password.AutoSize = true;
             this.label_password.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label_password.Location = new System.Drawing.Point(36, 98);
+            this.label_password.Location = new System.Drawing.Point(52, 98);
             this.label_password.Name = "label_password";
             this.label_password.Size = new System.Drawing.Size(58, 14);
             this.label_password.TabIndex = 1;
@@ -60,7 +61,7 @@
             // 
             this.textBox_username.BackColor = System.Drawing.SystemColors.Control;
             this.textBox_username.ForeColor = System.Drawing.Color.Firebrick;
-            this.textBox_username.Location = new System.Drawing.Point(136, 29);
+            this.textBox_username.Location = new System.Drawing.Point(159, 29);
             this.textBox_username.Name = "textBox_username";
             this.textBox_username.Size = new System.Drawing.Size(152, 20);
             this.textBox_username.TabIndex = 2;
@@ -69,7 +70,7 @@
             // 
             this.textBox_password.BackColor = System.Drawing.SystemColors.Control;
             this.textBox_password.ForeColor = System.Drawing.Color.Firebrick;
-            this.textBox_password.Location = new System.Drawing.Point(136, 92);
+            this.textBox_password.Location = new System.Drawing.Point(159, 92);
             this.textBox_password.Name = "textBox_password";
             this.textBox_password.Size = new System.Drawing.Size(152, 20);
             this.textBox_password.TabIndex = 3;
@@ -79,11 +80,14 @@
             // 
             this.btn_Login.BackColor = System.Drawing.Color.Silver;
             this.btn_Login.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Login.Image = ((System.Drawing.Image)(resources.GetObject("btn_Login.Image")));
+            this.btn_Login.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.btn_Login.Location = new System.Drawing.Point(39, 170);
             this.btn_Login.Name = "btn_Login";
-            this.btn_Login.Size = new System.Drawing.Size(100, 39);
+            this.btn_Login.Size = new System.Drawing.Size(144, 56);
             this.btn_Login.TabIndex = 4;
             this.btn_Login.Text = "Đăng nhập";
+            this.btn_Login.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             this.btn_Login.UseVisualStyleBackColor = true;
             this.btn_Login.Click += new System.EventHandler(this.btn_Login_Click);
             // 
@@ -91,11 +95,14 @@
             // 
             this.btn_Exit.BackColor = System.Drawing.Color.Silver;
             this.btn_Exit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Exit.Image = ((System.Drawing.Image)(resources.GetObject("btn_Exit.Image")));
+            this.btn_Exit.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.btn_Exit.Location = new System.Drawing.Point(217, 170);
             this.btn_Exit.Name = "btn_Exit";
-            this.btn_Exit.Size = new System.Drawing.Size(90, 39);
+            this.btn_Exit.Size = new System.Drawing.Size(126, 56);
             this.btn_Exit.TabIndex = 5;
             this.btn_Exit.Text = "Thoát";
+            this.btn_Exit.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             this.btn_Exit.UseVisualStyleBackColor = true;
             this.btn_Exit.Click += new System.EventHandler(this.btn_Exit_Click);
             // 
@@ -105,7 +112,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(364, 253);
+            this.ClientSize = new System.Drawing.Size(391, 253);
             this.Controls.Add(this.btn_Exit);
             this.Controls.Add(this.btn_Login);
             this.Controls.Add(this.textBox_password);
