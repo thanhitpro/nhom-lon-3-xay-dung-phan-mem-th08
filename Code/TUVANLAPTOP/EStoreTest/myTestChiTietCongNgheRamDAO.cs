4ad08fe3335f96ab3eea5bad35b0316577a711df
0812491 <0812491@8247ae65-48d3-9fc0-23df-3783ca5f111f>
2011-05-13 21:22:46 +0000

    Unit Test phần nhóm 24 Số lượng UT của nhóm khá nhiều nên sẽ tiếp tục cập nhật.

---
+++ Code/TUVANLAPTOP/EStoreTest/myTestChiTietCongNgheRamDAO.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using EStoreDTO;
+using NUnit.Framework;
+using EStoreDAO;
+
+namespace EStoreTest
+{
+    [TestFixture]
+    public class myTestChiTietCongNgheRamDAO
+    {
+        [Test]
+        public void TestLayChiTietCongNgheRam()
+        {
+            Assert.IsNotNull(myChiTietCongNgheRamDAO.LayChiTietCongNgheRam());
+        }
+
+        [Test]
+        public void TestThemCongNgheRAM()
+        {
+            myChiTietCongNgheRamDTO cnRAM = new myChiTietCongNgheRamDTO();
+            cnRAM.FHeSo = (float)1.0;
+            cnRAM.STenCongNgheRam = "DDR8";
+
+            Assert.AreEqual(true, myChiTietCongNgheRamDAO.ThemCongNgheRAM(cnRAM));
+
+            DataClasses1DataContext m_eStoreDataContext = new DataClasses1DataContext();
+
+            int maMax = m_eStoreDataContext.CHITIETCONGNGHERAMs.Max(it => it.MaChiTietCongNgheRAM);
+
+            m_eStoreDataContext.CHITIETCONGNGHERAMs.DeleteOnSubmit(m_eStoreDataContext.CHITIETCONGNGHERAMs.Single(it => it.MaChiTietCongNgheRAM == maMax));
+            m_eStoreDataContext.SubmitChanges();
+        }
+    }
+}
